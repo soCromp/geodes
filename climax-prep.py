scaling bug

climax-prep.py
@@ -19,9 +19,9 @@ trackspath1='/home/sonia/mcms/tracker/1940-2010/era5/out_era5/era5/mcms_era5_194
 trackspath2='/home/sonia/mcms/tracker/2010-2024/era5/out_era5/era5/FIXEDmcms_era5_2010_2024_tracks.txt'
 joinyear = 2010 # overlap for the track data
 
-use_temperature_2m = True
-use_geopotential = True #925hpa
-use_slp = False # whether to include slp channel
+use_temperature_2m = False
+use_geopotential = False #925hpa
+use_slp = True # whether to include slp channel
 use_windmag = False #include wind magnitude channel # NOTE THIS IS 500hPa
 use_winduv = True # include wind u and v components channels # NOTE THIS IS 500hPa
 use_temperature = True # temperature at 925hPa 
@@ -45,7 +45,7 @@ elif reg_id == 111:
     basin = 'pacific'
 basin = hemi + basin
 
-outpath = f'/mnt/data/sonia/climax-data/date/{basin}-multivar-fullcontext'
+outpath = f'/mnt/data/sonia/aurora-data/date/input-{basin}-multivar-fullcontext'
 
 # %%
 regmask = xr.open_dataset('/home/cyclone/regmask_0723_anl.nc')
@@ -83,7 +83,7 @@ varnames = [] # list of variables that will be included in this output dataset
 varlocs = {'temperature_2m': f'/mnt/data/sonia/cyclone/{grid}/temperature_2m',
            'geopotential': f'/mnt/data/sonia/cyclone/{grid}/geopotential',
            'slp': f'/mnt/data/sonia/cyclone/{grid}/slp', #'wind10m': '/home/cyclone/wind',
-           'wind': f'/mnt/data/sonia/cyclone/{grid}/wind_500hpa',
+           'wind_500hpa': f'/mnt/data/sonia/cyclone/{grid}/wind_500hpa',
            'temperature': f'/mnt/data/sonia/cyclone/{grid}/temperature',
            'humidity': f'/mnt/data/sonia/cyclone/{grid}/humidity',
            'topo': f'/mnt/data/sonia/cyclone/{grid}/slp/topo.nc'} # where the source data is stored 
@@ -117,7 +117,7 @@ if use_slp:
             return ds.sel(lat=lats, lon=lons, time=time)['slp']
     varfuncs['slp'] = f_slp
 if use_windmag:
-    varnames.append('wind')
+    varnames.append('wind_500hpa')
     def f_wind(ds, lats, lons, time=None):
         if time is None:
             u = ds.sel(lat=lats, lon=lons, pressure_level=500)['u'] # for 10m: [['u10', 'v10']] 
@@ -127,9 +127,9 @@ if use_windmag:
             v = ds.sel(lat=lats, lon=lons, time=time, pressure_level=500)['v']
         windmag = np.sqrt(u**2 + v**2).drop_vars('pressure_level')
         return windmag
-    varfuncs['wind'] = f_wind
+    varfuncs['wind_500hpa'] = f_wind
 if use_winduv:
-    varnames.append('wind')
+    varnames.append('wind_500hpa')
     def f_winduv(ds, lats, lons, time=None):
         # print(ds.sel(lat=lats, lon=lons))
         if time is None:
@@ -138,7 +138,7 @@ if use_winduv:
             data = ds.sel(lat=lats, lon=lons, time=time, pressure_level=500)[['u', 'v']] # for 10m: [['u10', 'v10']] 
         data = data.drop_vars('pressure_level')
         return data
-    varfuncs['wind'] = f_winduv
+    varfuncs['wind_500hpa'] = f_winduv
 if use_temperature:
     varnames.append('temperature')
     def f_temperature(ds, lats, lons, time=None):
@@ -241,11 +241,14 @@ def prep_point_fulldata(frame): # provide actual climate data, not patch plus cl
     else:
         data = data_vars[0]
 
-    new_lat = np.linspace(90,-90, 128) # north to south
-    new_lon = np.linspace(0, 360, 256, endpoint=False)
-    data_resized = data.interp(lat=new_lat, lon=new_lon, method="linear")
+    # new_lat = np.linspace(90,-90, 128) # north to south
+    # new_lon = np.linspace(0, 360, 256, endpoint=False)
+    # data_resized = data.interp(lat=new_lat, lon=new_lon, method="linear")
         
-    result = data_resized.to_array(dim="variable").values.squeeze()
+    # result = data_resized.to_array(dim="variable").values.squeeze()
+    result = data.to_array(dim="variable").values.squeeze()
+    # print(result.shape)
+    
     # print(data_resized, result.shape)
     if frame['split'] == 0:
         np.save(os.path.join(outpath, 'train', f"{frame['sid']}.np"), result)

train_3d.py
@@ -174,31 +174,9 @@ class CondDiffusionPipeline(DiffusionPipeline):
             z = noise[:, :, 0, :, :]
             sample[:, :, 0, :, :] = self.scheduler.add_noise(prompt, z, t_prev)
             
+        # clean prompt
+        sample[:, :, 0, :, :] = prompt
         return {"images": sample.cpu()}
-
-
-# def evaluate(samples, config, epoch, pipeline, device):
-#     # Sample some images from random noise (this is the backward diffusion process).
-#     # The default pipeline output type is `List[PIL.Image]`
-#     images = pipeline(
-#         torch.as_tensor(samples, dtype=config['dtype'], device=device),
-#         num_frames=config['frames'],
-#         generator=torch.Generator(device=device).manual_seed(config['seed']), 
-#         encoder_hidden_states=torch.zeros((config['train_batch_size'], 1, cross_attention_dim),
-#                                           device=device)
-#         # Use a separate torch generator to avoid rewinding the random state of the main training loop
-#     )['images']
-    
-#     # output from model is on [-1,1]  scale; convert to [0,255]
-#     images = 255/2 * ( 1+np.array(images) )
-    
-#     samples_dir = os.path.join(config['checkpoint_dir'], config['name'], "training_samples")
-#     os.makedirs(samples_dir, exist_ok=True)
-    
-#     for i in range(len(images)):
-#         for t in range(config['frames']):
-#             frame = Image.fromarray(images[i, :, t, :, :].squeeze()).convert('P')
-#             frame.save(os.path.join(samples_dir, f'{epoch:04d}_s{i:02d}_t{t:02d}.png'))
  
     
 def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_scheduler):
@@ -261,7 +239,7 @@ def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_s
             with accelerator.accumulate(model):
                 noise_pred = model(noisy_images, timesteps, encoder_hidden_states=zeros,
                                     return_dict=False)[0]
-                loss = F.mse_loss(noise_pred[:,:,1:,:,:], noise[:,:,1:,:,:]) # skip zeroth/prompt frame
+                loss = F.mse_loss(noise_pred, noise)
                 accelerator.backward(loss)
 
                 if accelerator.sync_gradients:

utils/dataset.py
@@ -228,18 +228,18 @@ class VideoDataset(Dataset):
         self.folders = [f for f in os.listdir(self.base_folder) if \
             os.path.isdir(os.path.join(self.base_folder, f))]#[:10]
         
-        data = [] # B C F H W
+        data = [] # want B C T H W
         for folder in self.folders:
             frames = []
             for i in range(self.sample_frames):
-                frame = np.load(os.path.join(self.base_folder, folder, f'{i}.npy'))
+                frame = np.load(os.path.join(self.base_folder, folder, f'{i}.npy')) # H W (C)
                 frames.append(frame)
-            data.append(np.stack(frames, axis=0))
-        self.data = np.stack(data, axis=0)
-        if self.data.ndim == 4:
-            self.data = np.expand_dims(self.data, axis=1) # add channel dim if missing
+            data.append(np.stack(frames, axis=0)) # T H W (C)
+        self.data = np.stack(data, axis=0) # B T H W (C)
+        if self.data.ndim == 4: # add channel dim if missing
+            self.data = np.expand_dims(self.data, axis=1) # now B C T H W
         else:
-            self.data = np.transpose(self.data, (0, 4, 1, 2, 3)) # B C F H W
+            self.data = np.transpose(self.data, (0, 4, 1, 2, 3)) # B C T H W
         assert self.data.shape[3] == self.height and self.data.shape[4] == self.width
         
         with open(os.path.join(self.base_folder, '../channels.txt'), 'r') as f:
@@ -265,34 +265,22 @@ class VideoDataset(Dataset):
             shape (batch, channels, time, H, W).
         """
         return {'pixel_values': self.data[idx], 'name': self.folders[idx]}
-        # # Randomly select a folder (representing a video) from the base folder
-        # chosen_folder = self.folders[idx] # random.choice(self.folders)
-        # folder_path = os.path.join(self.base_folder, chosen_folder)
-        # frames = sorted(os.listdir(folder_path))[:self.sample_frames] #paths
-        # frames = [np.load(os.path.join(folder_path, f)) for f in frames] #frame matrices
-
-        # # Initialize a tensor to store the pixel values (1 in batch size dimension for dataloader)
-        # pixel_values = torch.empty((1, self.sample_frames, self.height, self.width))
-
-        # # Load and process each frame
-        # for i, frame in enumerate(frames):
-        #     frame_log = np.log1p(frame)
-        #     img_tensor = torch.from_numpy(frame_log).float()
-        #     img_tensor[img_tensor.isnan()] = 0.0
-        #     img_norm = 2.0 * (img_tensor - self.min) / (self.max - self.min) - 1.0
-        #     img_norm = torch.clamp(img_norm, -1.0, 1.0)
-            
-        #     # Normalize the image by scaling pixel values to [-1, 1]
-        #     # img_normalized = 2 * (img_tensor - self.min) / (self.max - self.min) - 1
-        #     # img_normalized[img_normalized<-1] = -1 # in case of rounding errors
-        #     # img_normalized[img_normalized>1] = 1
-
-        #     pixel_values[:, i, :, :] = img_norm
-            
-        # return {'pixel_values': pixel_values, 'name': chosen_folder}
 
 
     def denormalize(self, x):
         if x.ndim == 4: # ensure theres a batch dim
             x = x.unsqueeze(0)
-        return self.normalizer.denormalize(x)
+        
+        if isinstance(x, torch.Tensor):
+            # clamp to prevent linear/exponential explosion of diffusion overshoots
+            x_out = torch.clamp(x.clone(), -1.0, 1.0) 
+        else:
+            x_out = np.clip(x.copy(), -1.0, 1.0)
+            
+        x = self.normalizer.denormalize(x)
+        
+        # go from B C T H W to B T H W C
+        if isinstance(x, torch.Tensor): 
+            x = x.permute(0, 2, 3, 4, 1)
+        else:
+            x = np.permute_dims(x, (0, 2, 3, 4, 1))