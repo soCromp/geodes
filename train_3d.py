21bb1e46d4173f33aee22ab61d781430baffc9d2
cromp <cromp@ap2001.chtc.wisc.edu>
2026-04-11 17:44:34 -0500

    fix eta passing

--- train_3d.py
+++ train_3d.py
@@ -192,7 +192,7 @@ class CondDiffusionPipeline(DiffusionPipeline):
         
         for i, t in enumerate(self.scheduler.timesteps):
             eps = self.unet(sample, t, encoder_hidden_states=encoder_hidden_states).sample
-            sample = self.scheduler.step(eps, t, sample, generator=generator).prev_sample
+            sample = self.scheduler.step(eps, t, sample, eta=kwargs.get('eta', 0.0), generator=generator).prev_sample
             t_prev = self.scheduler.timesteps[i+1] if i+1 < len(self.scheduler.timesteps) else t
             # z = torch.randn((batch_size, self.unet.config['in_channels'], config['image_size'], config['image_size']), 
             #                 device=device, dtype=config['dtype'], generator=generator)
